599680747252e00a453ed14d2abefd570c646483
Katerina Bouzouki <katerinampouzouki@gmail.com>
2025-07-22 17:12:19 +0300

    gsfs

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,15 +1,35 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Φόρτωσε το CSV (προσαρμόζεις το όνομα του αρχείου)
-df = pd.read_csv(r"C:\Users\PC\Downloads\deutsche_bank_financial_performance.csv")
+df = pd.read_csv(r"C:\Users\PC\Desktop\supermarket_sales.csv", sep=";")
 
-# Δες ποιες στήλες υπάρχουν
-print(df.columns.tolist())
+df['Sales'] = pd.to_numeric(df['Sales'], errors='coerce')
 
-# Υπολόγισε δείκτες (όνομα στηλών ανάλογα με το dataset)
-df["NetProfitMargin"] = df["Net Income"] / df["Revenue"]
-df["ROE"] = df["Net Income"] / df["Total Shareholder Equity"]
+print(df.head())
 
-# Αποθήκευσε το τελικό αρχείο
-df.to_csv("financial_with_ratios.csv", index=False)
-print("Οι δείκτες υπολογίστηκαν και το αρχείο deutsche_bank_financial_performance.csv αποθηκεύτηκε!")
+print(df.dtypes) 
+
+total_sales = df['Sales'].sum()
+print("Total Sales: ", total_sales)
+
+sales_by_city = df.groupby('City')['Sales'].sum().sort_values(ascending=False)
+print("Sales by city: ")
+print(sales_by_city.head(10))
+
+top_products = df.groupby('Sub-Category')['Sales'].sum().sort_values(ascending=False)
+top10_cities = sales_by_city.head(10)
+print("Top products: ")
+print(top10_cities)
+
+plt.figure(figsize=(10,6))
+sns.barplot(x=top_products.index, y=top_products.values, palette="viridis")
+plt.xticks(rotation=45)
+plt.title("Total Sales by Sub-Category")
+plt.ylabel("Sales")
+plt.xlabel("Sub-Category")
+plt.tight_layout()
+plt.show()
+
+
+
